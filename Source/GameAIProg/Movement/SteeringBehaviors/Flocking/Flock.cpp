41844b875534adaee5a1d980cad4bd1bc1cf780d
Sooi De Peuter <sooi.de.peuter@student.howest.be>
2026-03-03 20:35:07 +0100

    Flocking Initialization

--- Source/GameAIProg/Movement/SteeringBehaviors/Flocking/Flock.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Flocking/Flock.cpp
@@ -16,7 +16,26 @@ Flock::Flock(
 {
 	Agents.SetNum(FlockSize);
 
- // TODO: initialize the flock and the memory pool
+	pNeighbors.Reserve(50); //should be enough considering the range
+
+	//blended steering
+	std::vector<BlendedSteering::WeightedBehavior> birdBehavior{};
+	birdBehavior.reserve(5);
+	birdBehavior.emplace_back( pCohesionBehavior.get(), 0.6f );
+	birdBehavior.emplace_back( pSeparationBehavior.get(), 1.5f );
+	birdBehavior.emplace_back( pVelMatchBehavior.get(), 0.8f );
+	birdBehavior.emplace_back( pSeekBehavior.get(), 0.0f );
+	birdBehavior.emplace_back( pWanderBehavior.get(), 0.4f );
+
+	pBlendedSteering = std::make_unique<BlendedSteering>(birdBehavior);
+
+	//priority steering
+	std::vector<ISteeringBehavior*> evadeBehavior{};
+	evadeBehavior.reserve(2);
+	evadeBehavior.emplace_back(pEvadeBehavior);
+	evadeBehavior.emplace_back(pBlendedSteering);
+
+	pPrioritySteering = std::make_unique<PrioritySteering>(evadeBehavior);
 }
 
 Flock::~Flock()
@@ -26,11 +45,11 @@ Flock::~Flock()
 
 void Flock::Tick(float DeltaTime)
 {
- // TODO: update the flock
- // TODO: for every agent:
-  // TODO: register the neighbors for this agent (-> fill the memory pool with the neighbors for the currently evaluated agent)
-  // TODO: update the agent (-> the steeringbehaviors use the neighbors in the memory pool)
-  // TODO: trim the agent to the world
+  //TODO: update the flock
+  //TODO: for every agent:
+  //TODO: register the neighbors for this agent (-> fill the memory pool with the neighbors for the currently evaluated agent)
+  //TODO: update the agent (-> the steeringbehaviors use the neighbors in the memory pool)
+  //TODO: trim the agent to the world
 }
 
 void Flock::RenderDebug()
@@ -81,7 +100,26 @@ void Flock::ImGuiRender(ImVec2 const& WindowPos, ImVec2 const& WindowSize)
 		ImGui::Text("Behavior Weights");
 		ImGui::Spacing();
 
-  // TODO: implement ImGUI sliders for steering behavior weights here
+		ImGuiHelpers::ImGuiSliderFloatWithSetter("Cohesion",
+			pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight, 0.f, 1.f,
+			[this](float InVal) { pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight = InVal; }, "%.2f");
+
+		ImGuiHelpers::ImGuiSliderFloatWithSetter("Separation",
+			pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight, 0.f, 1.f,
+			[this](float InVal) { pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight = InVal; }, "%.2f");
+
+		ImGuiHelpers::ImGuiSliderFloatWithSetter("VelMatch",
+			pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight, 0.f, 1.f,
+			[this](float InVal) { pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight = InVal; }, "%.2f");
+
+		ImGuiHelpers::ImGuiSliderFloatWithSetter("Seek",
+			pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight, 0.f, 1.f,
+			[this](float InVal) { pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight = InVal; }, "%.2f");
+
+		ImGuiHelpers::ImGuiSliderFloatWithSetter("Wander",
+			pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight, 0.f, 1.f,
+			[this](float InVal) { pBlendedSteering->GetWeightedBehaviorsRef()[0].Weight = InVal; }, "%.2f");
+
 		//End
 		ImGui::End();
 	}
@@ -97,26 +135,46 @@ void Flock::RenderNeighborhood()
 #ifndef GAMEAI_USE_SPACE_PARTITIONING
 void Flock::RegisterNeighbors(ASteeringAgent* const pAgent)
 {
- // TODO: Implement
+	int index{};
+	for( ASteeringAgent* agent: Agents)
+	{
+		if ((agent->GetPosition() - pAgent->GetPosition()).Length() < NeighborhoodRadius &&
+			agent != pAgent)
+		{
+			pNeighbors[index] = agent;
+			index++;
+		}
+	}
+	NrOfNeighbors = index;
 }
 #endif
 
 FVector2D Flock::GetAverageNeighborPos() const
 {
-	FVector2D avgPosition = FVector2D::ZeroVector;
+	FVector2D averagePosition = FVector2D::ZeroVector;
 
- // TODO: Implement
+	for (int index{}; index < NrOfNeighbors; index++)
+	{
+		averagePosition += pNeighbors[index]->GetPosition();
+	}
+
+	averagePosition /= NrOfNeighbors;
 	
-	return avgPosition;
+	return averagePosition;
 }
 
 FVector2D Flock::GetAverageNeighborVelocity() const
 {
-	FVector2D avgVelocity = FVector2D::ZeroVector;
+	FVector2D averageVelocity = FVector2D::ZeroVector;
 
- // TODO: Implement
+	for (int index{}; index < NrOfNeighbors; index++)
+	{
+		averageVelocity += pNeighbors[index]->GetLinearVelocity();
+	}
 
-	return avgVelocity;
+	averageVelocity /= NrOfNeighbors;
+
+	return averageVelocity;
 }
 
 void Flock::SetTarget_Seek(FSteeringParams const& Target)
--- Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.cpp
@@ -6,13 +6,50 @@
 
 //*******************
 //COHESION (FLOCKING)
-SteeringOutput Cohesion::CalculateSteering(float deltaT, ASteeringAgent& pAgent)
+SteeringOutput Cohesion::CalculateSteering(float deltaT, ASteeringAgent& Agent)
 {
-	return SteeringOutput{};
+	//make an output object
+	SteeringOutput result;
+
+	//set Velocity
+	result.LinearVelocity = pFlock->GetAverageNeighborPos() - Agent.GetPosition();
+
+	return result;
 }
 
 //*********************
 //SEPARATION (FLOCKING)
+SteeringOutput Separation::CalculateSteering(float deltaT, ASteeringAgent& Agent)
+{
+	//make an output object
+	SteeringOutput result;
+	FVector2D averageDistance2D{};
+
+	for (ASteeringAgent* pAgent : pFlock->GetNeighbors())
+	{
+		const FVector2D distance2D{ pAgent->GetPosition() - Agent.GetPosition() };
+		const float distance{ distance2D.Length() };
+		const float inverseDistance{ 1.f / distance };
+
+		averageDistance2D += distance2D * inverseDistance;
+	}
+	averageDistance2D /= pFlock->GetNrOfNeighbors();
+
+	result.LinearVelocity = -averageDistance2D + Agent.GetPosition();
+
+	return result;
+}
 
 //*************************
 //VELOCITY MATCH (FLOCKING)
+
+SteeringOutput VelocityMatch::CalculateSteering(float deltaT, ASteeringAgent& pAgent)
+{
+	//make an output object
+	SteeringOutput result;
+
+	//set Velocity
+	result.LinearVelocity = pFlock->GetAverageNeighborVelocity();
+
+	return result;
+}
