add91d73a5be4bcd4e93c59a545112b39f4486a4
Sooi De Peuter <sooi.de.peuter@student.howest.be>
2026-04-09 15:44:18 +0200

    implemented step 1 of week 6

--- Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
+++ Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
@@ -48,12 +48,65 @@ int GameAI::NavGraph::GetNodeIdFromEdgeIndex(int EdgeIdx) const
 
 void GameAI::NavGraph::CreateNavigationGraph()
 {
-	//1. Go over all the edges of the navigation mesh and create nodes
-			// Create node here
-
-	//2. Create connections now that every node is created	
-		//2 valid nodes -> 1 connection
-		//3 valid nodes -> 3 connections
+	// Go over all the edges of the navigation mesh and create nodes
+	for (int index{}; index <  pNavPoly->GetEdges().size(); index++)
+	{
+		const TriPolygon::Edge* Line = &pNavPoly->GetEdges()[index];
 		
-	//3. Set the connections cost to the actual distance
+		//count touching triangles
+		int triangleCount{};
+		for (auto const & triangle : pNavPoly->GetTriangles())
+		{
+			if (triangle.HasEdge(*Line))
+			{
+				triangleCount++;
+			}
+		}
+		
+		//if in between two triangles
+		if (triangleCount >= 2)
+		{
+			const FVector2D Position{Line->GetP1(*pNavPoly) * 0.5 + Line->GetP2(*pNavPoly) * 0.5};
+			this->AddNode(std::make_unique<NavGraphNode>{Node{Position}, index});
+		}
+	}
+
+	// Create connections now that every node is created
+	for (auto const Triangle& Triangle : pNavPoly->GetTriangles())
+	{
+		std::array<TriPolygon::Edge, 3> Edges = Triangle.GetEdges();
+		
+		//store node indexes
+		std::vector<int> NodeIndexes{};
+		NodeIndexes.reserve(Edges.size());
+		for (TriPolygon::Edge Edge : Edges)
+		{
+			auto EdgeIndex = pNavPoly->FindEdgeIndex(Edge);
+			if (!EdgeIndex.has_value())
+			{
+				continue;
+			}
+			
+			int nodeId = GetNodeIdFromEdgeIndex(EdgeIndex.value());
+			if (nodeId != Graphs::InvalidNodeId)
+			{
+				NodeIndexes.emplace_back(nodeId);
+			}
+		}
+		
+		//make connections and set their weight
+		if (NodeIndexes.size() == 2)
+		{
+			this->AddConnection(std::make_unique<Connection>{NodeIndexes[0], NodeIndexes[1]});
+		}
+		else if (NodeIndexes.size() == 3)
+		{
+			this->AddConnection(std::make_unique<Connection>{NodeIndexes[0], NodeIndexes[1]});
+			this->AddConnection(std::make_unique<Connection>{NodeIndexes[1], NodeIndexes[2]});
+			this->AddConnection(std::make_unique<Connection>{NodeIndexes[2], NodeIndexes[0]});
+		}
+	}
+	
+	//set all weights
+	SetConnectionCostsToDistances();
 }
